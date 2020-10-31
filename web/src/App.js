cdb82643754ddb53988e5ad93d2aa74212d2ee71
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-10-31 10:18:37 -0300

    initial commit

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu site</h1>
+      <p>Uau, como meu site é belo e organizado...</p>
+    </div>
+  );
+}
+
+export default App;
