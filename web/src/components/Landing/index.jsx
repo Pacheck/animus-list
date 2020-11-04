458dac112f7009432fb78dc699ef8c0c3fd8cd7b
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-11-04 17:53:18 -0300

    finally resolved the api bug

--- web/src/components/Landing/index.jsx
+++ web/src/components/Landing/index.jsx
@@ -1,18 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Navigator from '../Navigator';
 
 import useAPI from '../../hooks/api.js';
 
+import imageTest1 from '../../assets/carrousel-teste1.png';
+import imageTest2 from '../../assets/carrousel-teste2.png';
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 const Landing = () => {
+
     const api = useAPI('anime/1/pictures');
-    console.log(api)
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        api.then(res => setImages(res))
+    }, [api])
+
+    console.log(images)
+
     return (
         <div className="landing-page">
-            <Navigator />  
+            <Navigator />
+            <Carousel>
+                <div>
+                    <img src={imageTest1} alt="testando"/>
+                    <p className="legend">Legend 1</p>
+                </div>
+                
+                <div>
+                    <img src={imageTest2} alt="testando"/>
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src={imageTest1} alt="testando"/>
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={imageTest2} alt="testando"/>
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
         </div>
     )
 }
--- web/src/hooks/api.js
+++ web/src/hooks/api.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react';
 
 import axios from 'axios';
 
-export default function (path) {
+export default async (path) => {
   const [response, setResponse] = useState([]);
 
-  useEffect(async () => {
-    const pathURL = path ? `/${path}` : '';
-    const res = await axios.get(`https://api.jikan.moe/v3${pathURL}`);
+  useEffect(() => {
+    const fetchData = async (path) => {
+      const pathURL = path ? `/${path}` : '';
+      const result = await axios.get(`https://api.jikan.moe/v3${pathURL}`);
 
-    setResponse(res);
-  }, []);
+      setResponse(result.data.pictures);
+    };
+
+    fetchData(path);
+  }, [path]);
 
   return response;
-}
+};
